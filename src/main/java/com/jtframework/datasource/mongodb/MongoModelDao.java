3cb2c1d6690f860ebeb63c297c1b966ee29bd76b
huangHong <767980702@qq.com>
2020-12-11 16:52:26 +0800

    fix bug

--- src/main/java/com/jtframework/datasource/mongodb/MongoModelDao.java
+++ src/main/java/com/jtframework/datasource/mongodb/MongoModelDao.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 
 @Slf4j
-public abstract class MongoModelDao<T> extends ModelDaoServiceImpl {
+public abstract class MongoModelDao<T> extends ModelDaoServiceImpl implements MongoModelDaoService {
 
     public abstract MongodbService getMongoService();
 
