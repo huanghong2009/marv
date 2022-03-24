80ffdd1be5a867f43948202be3e3102aafcd71fe
黄鸿 <>
2022-03-24 14:24:49 +0800

    丢失了方法

--- src/main/java/com/jtframework/utils/BaseUtils.java
+++ src/main/java/com/jtframework/utils/BaseUtils.java
@@ -436,6 +436,47 @@ public final class BaseUtils {
 
 
     /**
+     * 创建指定数量的随机字符串
+     *
+     * @param length
+     * @return
+     */
+    public static String getRandCode(int length) {
+        return getRandCode(true, length);
+    }
+
+    /**
+     * 创建指定数量的随机字符串
+     *
+     * @param numberFlag 是否是数字
+     * @param length
+     * @return
+     */
+    public static String getRandCode(boolean numberFlag, int length) {
+        String retStr = "";
+        String strTable = numberFlag ? "1234567890" : "1234567890abcdefghijkmnpqrstuvwxyz";
+        int len = strTable.length();
+        boolean bDone = true;
+        do {
+            retStr = "";
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                double dblR = Math.random() * len;
+                int intR = (int) Math.floor(dblR);
+                char c = strTable.charAt(intR);
+                if (('0' <= c) && (c <= '9')) {
+                    count++;
+                }
+                retStr += strTable.charAt(intR);
+            }
+            if (count >= 2) {
+                bDone = false;
+            }
+        } while (bDone);
+        return retStr;
+    }
+
+    /**
      * 校验是不是手机号
      *
      * @param mobiles
