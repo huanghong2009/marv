69766c41146f0a485051157b420243c752fab5e6
黄鸿 <>
2022-06-14 11:46:11 +0800

    fix bug

--- src/main/java/com/jtframework/datasource/mongodb/MongodbGroupDto.java
+++ src/main/java/com/jtframework/datasource/mongodb/MongodbGroupDto.java
@@ -1,14 +1,14 @@
 package com.jtframework.datasource.mongodb;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@NoArgsConstructor
+
 @Data
 public class MongodbGroupDto<T> extends MongodbParamsDTO{
+
 
     /**
      * 分组字段
@@ -40,6 +40,14 @@ public class MongodbGroupDto<T> extends MongodbParamsDTO{
 
     private Class<T> resultClass;
 
+
+    public MongodbGroupDto(){
+        this.groupFiledNames = new HashSet<>();
+    }
+
+    public void setGroupFiledName(String groupFiledName) {
+        this.groupFiledNames.add(groupFiledName);
+    }
 
     public MongodbGroupDto(String groupFiledName, String sortFiledName, Boolean asc, Set<String> returnFiledNames,Class<T> resultClass) {
         this.groupFiledNames = new HashSet<>();
